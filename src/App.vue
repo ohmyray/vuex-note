0383e995ead167089d780d47dd7fb486e43c5974
ohmyray <ohmyray@163.com>
2020-02-15 20:08:45 +0800

    feat： 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-addition></my-addition>
+    <hr>
+    <my-subtraction></my-subtraction>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Addition from './components/Addition'
+import Subtraction from './components/Subtraction'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
+  },
+  data () {
+    return {
+
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
